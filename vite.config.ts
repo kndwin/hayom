3482ca475e188c848a4eacce3414e69b2312ebad
kndwin <tnkevino@gmail.com>
2024-07-16 10:24:17 +1000

    feat: add PWA (just caching)

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,29 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  optimizeDeps: {
-    exclude: ["@evolu/common-web"],
-    include: ["react-dom", "path-browserify", "fast-querystring"],
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: `[name][hash].js`,
-        chunkFileNames: `[name][hash].js`,
-        assetFileNames: `[name][hash].[ext]`,
-      },
-    },
-  },
-  worker: {
-    format: "es",
-  },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+	optimizeDeps: {
+		exclude: ["@evolu/common-web"],
+		include: ["react-dom", "path-browserify", "fast-querystring"],
+	},
+	build: {
+		rollupOptions: {
+			output: {
+				entryFileNames: `[name][hash].js`,
+				chunkFileNames: `[name][hash].js`,
+				assetFileNames: `[name][hash].[ext]`,
+			},
+		},
+	},
+	worker: {
+		format: "es",
+	},
+	plugins: [react(), VitePWA({ registerType: 'autoUpdate' })],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
 });
