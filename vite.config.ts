4eb64ffe1b9891d8ae109646cf4d68b2bee5fb3f
kndwin <tnkevino@gmail.com>
2024-07-16 10:42:49 +1000

    fix: set up minimal requirement for PWA

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,42 @@ export default defineConfig({
 	worker: {
 		format: "es",
 	},
-	plugins: [react(), VitePWA({ registerType: 'autoUpdate' })],
+	plugins: [react(), VitePWA({
+		registerType: 'autoUpdate',
+
+		includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+		manifest: {
+			name: 'My Awesome App',
+			short_name: 'MyApp',
+			description: 'My Awesome App description',
+			theme_color: '#ffffff',
+			icons: [
+				{
+					src: 'pwa-192x192.png',
+					sizes: '192x192',
+					type: 'image/png'
+				},
+				{
+					src: 'pwa-512x512.png',
+					sizes: '512x512',
+					type: 'image/png'
+				},
+				{
+					src: 'pwa-512x512.png',
+					sizes: '512x512',
+					type: 'image/png',
+					purpose: 'any'
+				},
+				{
+					src: 'pwa-512x512.png',
+					sizes: '512x512',
+					type: 'image/png',
+					purpose: 'maskable'
+				}
+			]
+		}
+
+	})],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
